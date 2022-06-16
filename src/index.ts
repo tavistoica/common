1d8e44cfda08aacb2c514c57e580d04243fb1223
tavistoica <stoicaoctavian25@yahoo.com>
2022-06-16 17:14:40 +0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/require-seller";
+export * from "./middlewares/require-user";
 export * from "./middlewares/validate-request";
 
 export * from "./events/base-listener";
